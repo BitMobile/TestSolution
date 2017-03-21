309d4a5d7fab34f7dcb5f1d56a450d2f3594485d
TaV <tav0x222@gmail.com>
2017-03-21 15:09:22 +0300

    PLA-1728, PLA-1794, PLA-1795

--- Test/Test/Module/BusinessProcess.cs
+++ Test/Test/Module/BusinessProcess.cs
@@ -6,80 +6,80 @@ using XmlDocument = BitMobile.ClientModel3.XmlDocument;
 
 namespace Test
 {
-    public class BusinessProcess
-    {
-        private static XmlDocument doc;
-        private static readonly Stack stackNodes = new Stack();
-        private static readonly Stack stackScreens = new Stack();
+	public class BusinessProcess
+	{
+		private static XmlDocument doc;
+		private static readonly Stack stackNodes = new Stack();
+		private static readonly Stack stackScreens = new Stack();
 
-        public static void Init()
-        {
-            doc = new XmlDocument();
-            doc.Load(Application.GetResourceStream("BusinessProcess.BusinessProcess.xml"));
+		public static void Init()
+		{
+			doc = new XmlDocument();
+			doc.Load(Application.GetResourceStream("BusinessProcess.BusinessProcess.xml"));
 
-            var firstStepName = doc.DocumentElement.ChildNodes[0].ChildNodes[0].Attributes["Name"].Value;
-            MoveTo(firstStepName);
-        }
+			var firstStepName = doc.DocumentElement.ChildNodes[0].ChildNodes[0].Attributes["Name"].Value;
+			MoveTo(firstStepName);
+		}
 
-        private static void MoveTo(string stepName)
-        {
-            var n =
-                doc.DocumentElement.SelectSingleNode(string.Format("//BusinessProcess/Workflow/Step[@Name='{0}']",
-                    stepName));
-            var stepController = n.Attributes["Controller"].Value;
-            var styleSheet = n.Attributes["StyleSheet"].Value;
+		private static void MoveTo(string stepName)
+		{
+			var n =
+				doc.DocumentElement.SelectSingleNode(string.Format("//BusinessProcess/Workflow/Step[@Name='{0}']",
+					stepName));
+			var stepController = n.Attributes["Controller"].Value;
+			var styleSheet = n.Attributes["StyleSheet"].Value;
 
-            var scr = GetScreenByControllerName(stepController);
+			var scr = GetScreenByControllerName(stepController);
 
-            stackScreens.Push(scr);
-            stackNodes.Push(n);
+			stackScreens.Push(scr);
+			stackNodes.Push(n);
 
-            scr.LoadStyleSheet(Application.GetResourceStream(styleSheet));
-            scr.Show();
-        }
+			scr.LoadStyleSheet(Application.GetResourceStream(styleSheet));
+			scr.Show();
+		}
 
-        public static void DoAction(string actionName)
-        {
-            var currentNode = (XmlNode) stackNodes.Peek();
-            var n = currentNode.SelectSingleNode(string.Format("Action[@Name='{0}']", actionName));
-            var stepName = n.Attributes["NextStep"].Value;
-            MoveTo(stepName);
-        }
+		public static void DoAction(string actionName)
+		{
+			var currentNode = (XmlNode) stackNodes.Peek();
+			var n = currentNode.SelectSingleNode(string.Format("Action[@Name='{0}']", actionName));
+			var stepName = n.Attributes["NextStep"].Value;
+			MoveTo(stepName);
+		}
 
-        public static void DoBack()
-        {
-            //remove current
-            stackNodes.Pop();
-            stackScreens.Pop();
+		public static void DoBack()
+		{
+			//remove current
+			stackNodes.Pop();
+			stackScreens.Pop();
 
-            var scr = (Screen) stackScreens.Peek();
-            scr.Show();
-        }
+			var scr = (Screen) stackScreens.Peek();
+			scr.Show();
+		}
 
-        private static Screen GetScreenByControllerName(string name)
-        {
-            //return Screen.CreateScreen("Test." + name);
-            //full type name should be specified 
+		private static Screen GetScreenByControllerName(string name)
+		{
+			//return Screen.CreateScreen("Test." + name);
+			//full type name should be specified 
 
-            //var scr = Screen.CreateScreen("Test." + name); //full type name should be specified
-            Screen scr = (Screen)Application.CreateInstance("Test." + name); //full type name should be specified
-            Stream s = null;
-            var path = string.Format(@"Screen\{0}.xml", name);
-            try
-            {
-                s = Application.GetResourceStream(path); //try to find markup resource
-            }
-            catch
-            {
-                DConsole.WriteLine(string.Format("Resource {0} has not been found", path));
-            }
+			//var scr = Screen.CreateScreen("Test." + name); //full type name should be specified
+			Screen scr = (Screen) Application.CreateInstance("Test." + name); //full type name should be specified
+			Stream s = null;
+			var path = string.Format(@"Screen\{0}.xml", name);
+			try
+			{
+				s = Application.GetResourceStream(path); //try to find markup resource
+			}
+			catch
+			{
+				DConsole.WriteLine(string.Format("Resource {0} has not been found", path));
+			}
 
-            if (s != null)
-            {
-                scr.LoadFromStream(s);
-            }
+			if (s != null)
+			{
+				scr.LoadFromStream(s);
+			}
 
-            return scr;
-        }
-    }
+			return scr;
+		}
+	}
 }
